41936f6a7c34932bbdbb72e88b43534474539690
Vidit <65944284+Vidit-Ostwal@users.noreply.github.com>
2021-11-25 13:53:58 +0530

    Update Q46.cpp

--- Question/Leetcode/Medium/Q46.cpp
+++ Question/Leetcode/Medium/Q46.cpp
@@ -39,3 +39,46 @@ public:
         return get_permutations(nums,0,temp_vector,final_vector);
     }
 };
+
+
+
+
+
+
+/* Second appraoch of not passing the final_vector to the function call */
+void print(vector<int> temp_vector)
+{
+  for (int i = 0; i < temp_vector.size();i++)
+  {
+    cout << temp_vector[i]; 
+  }
+  cout << endl;
+}
+
+vector<vector<int>> get_permutations(vector<int>& nums, int index, vector<int> temp_vector)
+{
+  if (index == nums.size())
+  {
+    vector<vector<int>> final_vector1;
+    final_vector1.push_back(temp_vector);
+    return final_vector1;
+  }
+  vector<vector<int>> final_vector;
+  for(int i = 0; i < (temp_vector.size()+1);i++)
+  {
+    auto it = temp_vector.insert(temp_vector.begin()+i,nums[index]);
+    vector<vector<int>> temp_ve = get_permutations(nums,index+1,temp_vector);
+    final_vector.insert(final_vector.end(),temp_ve.begin(),temp_ve.end());
+    temp_vector.erase(it);
+  }
+  return final_vector;
+}
+  
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+      vector<vector<int>> final_vector;
+      vector<int> temp_vector;
+        return get_permutations(nums,0,temp_vector);
+    }
+};
