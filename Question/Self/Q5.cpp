8c02214ed9873cc4615377b0421e5b349056470b
Vidit <65944284+Vidit-Ostwal@users.noreply.github.com>
2021-11-26 18:03:55 +0530

    Update Q5.cpp

--- Question/Self/Q5.cpp
+++ Question/Self/Q5.cpp
@@ -1,3 +1,8 @@
+/* Idea is that whenever it hits edge of the matrix after that only one way to go so taht makes it a base condition 
+Here 5,5 to 1,1 and base condition when it hits 1,1 
+In this person can go only right or down.*/
+
+
 // Online C++ compiler to run C++ program online
 #include <iostream>
 
