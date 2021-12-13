eda1fb6515e20e2d438e84822e39b755192afd2b
Vidit <65944284+Vidit-Ostwal@users.noreply.github.com>
2021-12-13 19:15:33 +0530

    Time: 556 ms (5.02%), Space: 112.7 MB (5.02%) - LeetHub

---
+++ sort-list/sort-list.cpp
@@ -0,0 +1,133 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  
+  /* Merges two Sorted Linked List*/
+  ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+      ListNode* node1 = list1;
+      ListNode* node2 = list2;
+      
+      if (node1 == NULL )
+      {
+        return node2;
+      }
+      if (node2 == NULL )
+      {
+        return node1;
+      }
+      
+      ListNode* finalpointer = new ListNode();
+      ListNode* finaldefinedpointer = finalpointer;
+      while (node1 != NULL && node2 != NULL)
+      {
+        if (node1 -> val  <= node2 -> val)
+        {
+          finalpointer -> val = node1 -> val;
+          finalpointer -> next = new ListNode();
+          node1 = node1 -> next;
+          finalpointer = finalpointer -> next;
+        }
+        else
+        {
+          finalpointer -> val = node2 -> val;
+          finalpointer -> next = new ListNode();
+          node2 = node2 -> next;
+          finalpointer = finalpointer -> next;
+        }
+      }
+      
+      if (node1 == NULL && node2 != NULL)
+        {
+          while (node2 != NULL)
+          {
+            finalpointer -> val = node2 -> val;
+            node2 = node2 -> next;
+            if (node2 == NULL)
+            {
+              finalpointer-> next = NULL;
+            }
+            else
+            {
+              finalpointer -> next = new ListNode();
+              finalpointer = finalpointer -> next;
+            }
+          }
+        }
+        
+      if (node2 == NULL && node1 != NULL)
+        {
+          while (node1 != NULL)
+          {
+            finalpointer -> val = node1 -> val;
+            node1 = node1 -> next;
+            if (node1 == NULL)
+            {
+              finalpointer-> next = NULL;
+            }
+            else
+            {
+              finalpointer -> next = new ListNode();
+              finalpointer = finalpointer -> next;
+            }
+          }
+        }
+      return finaldefinedpointer;
+    }
+  
+  
+  void printlinkedList (ListNode* node)
+  {
+    while (node != NULL)
+    {
+      cout << node -> val ; 
+      node = node -> next;
+    }
+    cout << endl;
+  }
+  ListNode* middleNode(ListNode* head) {
+    cout << " Noooooo"  << endl;
+      ListNode* slow = head;
+      ListNode* fast = head;
+      ListNode* slowprev;
+      while (fast-> next != NULL)
+      {
+        if (fast -> next -> next != NULL)
+        {
+          fast = fast -> next -> next;
+        }
+        else
+        {
+          fast = fast -> next;
+        }
+        slowprev = slow;
+        slow = slow -> next;
+      }
+    slowprev -> next = NULL;
+    printlinkedList(head);
+    printlinkedList(slow);
+      return slow;
+    }
+
+    ListNode* sortList(ListNode* head){
+      cout << "Fuck Me" << endl;
+      if (head == NULL || head -> next == NULL)
+      {
+        return head;
+      }
+      ListNode* mid = middleNode(head);
+      
+      ListNode* left = sortList(head);
+      ListNode* right = sortList(mid);
+      ListNode* Head = mergeTwoLists(left,right);
+      return Head;
+    }
+};
