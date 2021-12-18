7bef37368182c06ba04e4f056bb394bab87cc3ed
Vidit <65944284+Vidit-Ostwal@users.noreply.github.com>
2021-12-19 00:00:17 +0530

    Time: 16 ms (59.30%), Space: 11.5 MB (31.34%) - LeetHub

---
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -0,0 +1,129 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+  /*index takes direct position*/
+  ListNode* get (ListNode* head, int index)
+  {
+    index--;
+    while (index != 0)
+    {
+      head = head-> next;
+      index--;
+    }
+    return head;
+  }
+  
+  /*work of reversing the linked list*/
+  ListNode* reverselinkedlist (ListNode* Head)
+  {
+    ListNode* prev = NULL;
+    ListNode* present = Head;
+    ListNode* future = present-> next;
+    
+    do 
+    {
+      present -> next = prev;
+      prev = present;
+      present = future;
+      if (future != NULL)
+      {
+        future = future -> next;
+      }
+    }while(present != NULL);
+    
+    return prev;
+  }
+  
+  ListNode* reversepart (ListNode* head, int left1, int right1)
+  {
+    /* starting from head and in between */
+    if (left1 == 1 && get(head,right1)-> next != NULL)
+    {
+      ListNode* left = get(head,left1);
+      ListNode* right = get(head,right1);
+      ListNode* future  = get(head,right1+1);
+      right-> next = NULL;
+      
+      ListNode* Head = reverselinkedlist(left);
+      ListNode* temp = Head;
+      while(temp -> next != NULL)
+      {
+        temp = temp-> next;
+      }
+      temp -> next = future;
+      return Head;
+    }
+    
+    /* Starting not from head and ending at the end */
+    if (left1 != 1 && get(head,right1)-> next == NULL)
+    {
+      ListNode* prev = get(head, left1-1);
+      ListNode* left = get(head,left1);
+      prev -> next = reverselinkedlist(left);
+      return head;
+    }
+    
+    /*In between cases */
+    ListNode* prev = get(head, left1-1);
+    ListNode* left = get(head,left1);
+    ListNode* right = get(head,right1);
+    ListNode* future  = get(head,right1+1);
+    right-> next = NULL;
+    
+    prev -> next = reverselinkedlist(left);
+    while(prev -> next != NULL)
+    {
+      prev = prev-> next;
+    }
+    prev-> next = future;
+    return head;
+  }
+  
+  ListNode* reverseBetween(ListNode* head, int left, int right)
+  {
+      
+       if (head == NULL || head -> next == NULL || left == right)
+      {
+        return head;
+      }
+      if (left == 1 && get(head,right) -> next == NULL)
+      {
+        return reverselinkedlist(head);
+      }
+        return reversepart (head,left,right);
+    }
+    
+  int length (ListNode* head)
+  {
+    int count = 0;
+    while (head != NULL)
+    {
+      count++;
+      head = head -> next;
+    }
+    return count;
+  }
+  
+  ListNode* reverseKGroup(ListNode* head, int k) {
+    int number_of_iteration = length(head)/k;
+    int left = 1;
+    int right = k;
+    
+    for (int i = 0; i < number_of_iteration; i++)
+    {
+      head = reverseBetween(head,left,right);
+      left = left + k;
+      right = right + k;
+    }
+    return head;
+    }
+};
