2bc467e88c8f2e11752c9f6b66b017d267224c9d
Vidit <65944284+Vidit-Ostwal@users.noreply.github.com>
2021-12-21 00:57:27 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (32.25%) - LeetHub

---
+++ pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+  
+  /*size of row is the row_number*/
+  /*Time Complexity = O(N) + O(N-1) .....  = O(N*N)
+  Space Complexity = O(N*N)
+  */
+  vector<int> make_new_row (vector<int> prev_row)
+  {
+    vector<int> final_vector;
+    
+    for (int i = 0; i < prev_row.size(); i++)
+    {
+      if (i == 0)
+      {
+        final_vector.push_back(prev_row[i]);
+      }
+      
+      if ( i > 0 )
+      {
+        final_vector.push_back(prev_row[i] + prev_row[i-1]);
+      }
+      
+      if (i == prev_row.size() -1)
+      {
+        final_vector.push_back(prev_row[i]);
+      }
+    }
+    return final_vector;
+  }
+  
+  
+    vector<vector<int>> generate(int numRows) {
+      vector<vector<int>> final_vector ;
+      vector<int> row_1 = {1};
+      final_vector.push_back(row_1);
+      if (numRows == 1)
+      {
+        return final_vector;
+      }
+      
+      for (int i = 0 ; i< numRows-1;i++)
+      {
+        final_vector.push_back(make_new_row(final_vector[i]));
+      }
+      return final_vector;
+    }
+  
+};
