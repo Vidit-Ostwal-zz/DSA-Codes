cf561235ef839596614fe5586140d88f040f7431
Vidit <65944284+Vidit-Ostwal@users.noreply.github.com>
2021-12-31 00:11:32 +0530

    Time: 97 ms (5.54%), Space: 31 MB (5.22%) - LeetHub

---
+++ subsets-ii/subsets-ii.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+  
+  vector<vector<int>> convertToSet(vector<vector<int>> prev_vector)
+{
+    // Declaring the  set
+   set<vector<int>> s(prev_vector.begin(),prev_vector.end());
+   vector<vector<int>> final_vector(s.begin(),s.end());
+    
+    return final_vector;
+}
+  
+  
+    /*Approach 1*/
+  vector<vector<int>> get_all_subsets(vector<int>&nums,int index,vector<int> prev_vector)
+  {
+    if (index == nums.size())
+    {
+      vector<vector<int>> temp_vector;
+      temp_vector.push_back(prev_vector);
+      return temp_vector;
+    }
+    
+    vector<vector<int>> first_vector = get_all_subsets(nums,index+1,prev_vector);
+   
+    prev_vector.push_back(nums[index]);
+    vector<vector<int>> second_vector = get_all_subsets(nums,index+1,prev_vector);
+    
+    first_vector.insert(first_vector.end(),second_vector.begin(),second_vector.end());
+    
+    return convertToSet(first_vector);
+  }
+  
+  
+  void get_all_subsets_void(vector<int>&nums,int index,vector<int> prev_vector,vector<vector<int>> & final_vector)
+  {
+    if (index == nums.size())
+    {
+      final_vector.push_back(prev_vector);
+      return ;
+    }
+    
+    get_all_subsets_void(nums,index+1,prev_vector,final_vector);
+   
+    prev_vector.push_back(nums[index]);
+    get_all_subsets_void(nums,index+1,prev_vector,final_vector);
+  }
+  
+  
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+      sort(nums.begin(),nums.end());
+        return convertToSet(get_all_subsets(nums,0,{}));
+    }
+};
