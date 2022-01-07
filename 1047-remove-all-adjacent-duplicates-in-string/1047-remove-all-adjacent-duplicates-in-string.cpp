fd1e1dcf4013442e898e11e6db3927339545f0d2
Vidit <viditostwal@gmail.com>
2022-01-07 18:07:17 +0530

    Time: 1257 ms (5.00%), Space: 780.8 MB (5.00%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -15,11 +15,7 @@ public:
     stack < char> st ;
     for (int i = 0 ; i < s.length(); i++)
     {
-      if (st.empty())
-      {
-        st.push(s[i]);
-      }
-      else if (st.top() == s[i])
+      if (!st.empty() && st.top() == s[i])
       {
         st.pop();
       }
