ddb82cd38b2ca319d7fd6019428e45f284f16935
Vidit <viditostwal@gmail.com>
2022-01-07 16:28:30 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (78.92%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    bool isValid(string s) {
+         stack <char> st;
+      for (int i = 0 ;i < s.length(); i++)
+      {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+        {
+          st.push(s[i]);
+        }
+        else
+        {
+          if (st.empty())
+          {
+            return false;
+          }
+        else if (s[i] == ')')
+        {
+          if (!st.empty() && st.top() != '(')
+          {
+            return false;
+          }
+          st.pop();
+        }
+        else if (s[i] == ']')
+        {
+          if (!st.empty() && st.top() != '[')
+          {
+            return false;
+          }
+          st.pop();
+        }
+        else if (s[i] == '}')
+        {
+          if (!st.empty() && st.top() != '{')
+          {
+            return false;
+          }
+          st.pop();
+        }
+          
+      }
+      }
+      
+      if (st.empty())
+      {
+              return true;
+      }
+      return false;
+    }
+};
