bf5b83b24f3d73eeb353448cc13079364fcc3c2f
Vidit <viditostwal@gmail.com>
2022-01-07 18:04:38 +0530

    Time: 1209 ms (5.00%), Space: 781.3 MB (5.00%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+  /*
+  Time Complexity and Space Complexity = O(N)
+  */
+  string optimized_solution(string s)
+  {
+    string  answer = "";
+    /*Handling the edge case*/
+    if (s.length() < 2)
+    {
+      return s;
+    }
+    
+    stack < char> st ;
+    for (int i = 0 ; i < s.length(); i++)
+    {
+      if (st.empty())
+      {
+        st.push(s[i]);
+      }
+      else if (st.top() == s[i])
+      {
+        st.pop();
+      }
+      else
+      {
+        st.push(s[i]);
+      }
+    }
+    while(!st.empty())
+    {
+      answer = st.top() + answer;
+      st.pop();
+    }
+    return answer;
+  }
+  
+  
+  
+    string removeDuplicates(string s) {
+        return optimized_solution(s);
+    }
+};
