f1445f42d2a693e6ceef99babbb2046e648809d6
Vidit <viditostwal@gmail.com>
2022-01-08 16:35:21 +0530

    Time: 611 ms (5.04%), Space: 162.4 MB (5.02%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -7,29 +7,35 @@ public:
   2> Candiadates are specifically different
   */
   
-  void get_valid_combinations(vector<int> candidates, int target, vector<vector<int>>&answer, vector<int> temp, int index)
+   vector<vector<int>> get_valid_combinations(vector<int> candidates, int target, vector<int> temp, int index)
   {
     /*Base Condition*/
     if (target == 0)
     {
+       vector<vector<int>> answer;
       answer.push_back(temp);
-      return;
+      return answer;
+    }
+     
+    if (target < 0 || index >= candidates.size())
+    {
+       vector<vector<int>> answer;
+        return answer;
     }
     
-    if (target < 0 || index >= candidates.size())
-      return;
     
-    
-    get_valid_combinations(candidates, target, answer,temp, index+1);
+    vector<vector<int>> vect1 = get_valid_combinations(candidates, target,temp, index+1);
+     
     temp.push_back(candidates[index]);
-    get_valid_combinations(candidates, target-candidates[index], answer,temp, index);
+    vector<vector<int>> vect2 = get_valid_combinations(candidates, target-candidates[index],temp, index);
+     
+     vect1.insert(vect1.end(),vect2.begin(),vect2.end());
+     return vect1;
   }
   
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>answer;
       vector<int> temp;
-      get_valid_combinations(candidates,target,answer,temp,0);
-      return answer;
+      return get_valid_combinations(candidates,target,temp,0);
     }
 };
