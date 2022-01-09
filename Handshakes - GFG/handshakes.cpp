bc6bcef6dc0c9a0632f9b27f61ce5e057bc9bd38
Vidit <viditostwal@gmail.com>
2022-01-09 14:06:17 +0530

    Added solution - LeetHub

---
+++ Handshakes - GFG/handshakes.cpp
@@ -0,0 +1,47 @@
+// { Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+public:
+int count_handshakes(int N)
+{
+    if (N <= 2)
+    {
+        return N-1;
+    }
+    int count = 0;
+    for (int i = 1; i < N;)
+    {
+        if (i == 1 || i == N-1)
+        {
+            count += count_handshakes(N-2);
+        }
+        else
+        {
+            count += count_handshakes(i-1)*count_handshakes(N -1 -i);
+        }
+        i = i+2;
+    }
+    return count;
+}
+    int count(int N){
+        return count_handshakes(N);
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.count(N) << endl;
+    }
+    return 0; 
+}  // } Driver Code Ends
