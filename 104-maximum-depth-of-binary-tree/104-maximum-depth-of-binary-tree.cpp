25d6dc11a5f0946dae3db936f6326cdcdc9f70fd
Vidit <viditostwal@gmail.com>
2022-01-14 18:59:49 +0530

    Time: 19 ms (10.88%), Space: 19 MB (14.70%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -0,0 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  
+  /* Recursive approach 
+  N = number of nodes in binary tree
+  Time Complexity = O(N)
+  Space Complexity = O(N) Height of recursion tree
+  */
+  int get_height(TreeNode* root)
+  {
+    if (root == NULL)
+      return 0;
+    
+    return 1 + max(get_height(root -> left),get_height(root -> right));
+  }
+  
+  /*Level Order Search*/
+  /* Iterative approach 
+  N = number of nodes in binary tree
+  Time Complexity = O(N)
+  Space Complexity = O(1)
+  */
+  int level_order (TreeNode* root)
+  {
+    queue<TreeNode*> q;
+    q.push(root);
+    int count = 0;
+    while (!q.empty())
+    {
+      int size = q.size();
+      for (int i = 0; i < size ;i++)
+      {
+        TreeNode* temp = q.front();
+        q.pop();
+        if (temp -> left != NULL)
+          q.push(temp -> left);
+        
+        if (temp -> right != NULL)
+          q.push(temp -> right);
+      }
+      count++;
+    }
+    return count;
+  }
+  
+    int maxDepth(TreeNode* root) {
+      if (root == NULL)
+      {
+        return 0;
+      }
+        return level_order(root);
+    }
+};
