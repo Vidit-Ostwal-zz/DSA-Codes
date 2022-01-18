05762b65e888321aa8aabd79ff8b21332731f578
Vidit <viditostwal@gmail.com>
2022-01-18 22:44:37 +0530

    Added solution - LeetHub

---
+++ Bottom View of Binary Tree - GFG/bottom-view-of-binary-tree.cpp
@@ -0,0 +1,170 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+vector <int> bottomView(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if(currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if(currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+ // } Driver Code Ends
+//Function to return a list containing the bottom view of the given tree.
+
+class Solution {
+    void findminmax(Node* root,int &min,int &max,int hd)
+    {
+        if (root == NULL) return;
+        
+        if (hd < min) min = hd;
+        if (hd > max) max = hd;
+        
+        if (root -> left != NULL) findminmax(root -> left,min,max,hd-1);
+        if (root -> right != NULL) findminmax(root -> right,min,max,hd+1);
+    }
+    
+    /*Level order Traversal along with storage*/
+    void getvertical(Node *root, unordered_map<int,vector<int>> &mp,int hd)
+    {
+        if (root == NULL) return ;
+        
+        /*first thing will store the node pointer and second one will contain
+        the horizontal distance value 
+        data value can be taken from the root*/
+        queue<pair<Node*,int>> q;
+        
+        q.push({root,hd});
+        
+        while (!q.empty())
+        {
+             root = q.front().first;
+             int updated_hd = q.front().second;
+             q.pop();
+             mp[updated_hd].push_back(root-> data);
+             if (root -> left != NULL) q.push({root -> left,updated_hd-1});
+             if (root -> right != NULL) q.push({root -> right,updated_hd +1});
+        }
+        
+    }
+  public:
+    vector <int> bottomView(Node *root) {
+        int min  = 0;
+        int max = 0;
+        findminmax(root,min,max,0);
+        
+        unordered_map <int,vector<int>> mp;
+        
+        getvertical (root,mp,0);
+        vector<int> final_vector;
+        for (int i = min ; i <= max ; i++)
+        {
+            final_vector.push_back(mp[i][mp[i].size()-1]);
+        }
+        return final_vector;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    string tc;
+    getline(cin, tc);
+    t=stoi(tc);
+    while(t--)
+    {
+        string s ,ch;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        vector <int> res = ob.bottomView(root);
+        for (int i : res) cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
+
+
+  // } Driver Code Ends
