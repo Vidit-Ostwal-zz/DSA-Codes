647507dec2a8672623b4d4b5dd0347b240d3e683
Vidit <viditostwal@gmail.com>
2022-01-20 11:11:50 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (48.07%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -23,20 +23,20 @@ public:
   
   int number_of_path_tabu(int m, int n)
   {
-      vector<vector<int>> dp (m+1,vector<int>(n+1,-1));
+      vector<vector<int>> dp (2,vector<int>(n+1,-1));
       
     for (int i = 0; i <= m; i++)
     {
       for (int j = 0; j <= n; j++)
       {
         if (i == 0 || j == 0)
-          dp[i][j] = 1;
+          dp[i%2][j] = 1;
         
         else
-          dp[i][j] = dp[i-1][j] + dp[i][j-1];
+          dp[i%2][j] = dp[(i-1)%2][j] + dp[i%2][j-1];
       }
     }
-    return dp[m][n];
+    return dp[m%2][n];
   }
   
   
