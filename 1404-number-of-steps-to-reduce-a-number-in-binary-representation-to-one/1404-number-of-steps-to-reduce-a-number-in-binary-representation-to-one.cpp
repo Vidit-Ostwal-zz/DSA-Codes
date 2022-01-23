024c5ef0d55461fee941e43f33f9c0eb817a8cb7
Vidit <viditostwal@gmail.com>
2022-01-23 18:22:37 +0530

    Time: 35 ms (5.32%), Space: 9.7 MB (8.51%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -0,0 +1,40 @@
+class Solution {
+  string add1 (string s)
+  {
+    int index = s.length()-1;
+    
+    while ( index >= 0 && s[index] - '0' == 1)
+    {
+      s[index] = '0';
+      index--;
+    }
+    if (index >= 0)
+    {
+      s[index] = '1';
+      return s;
+    }
+    else
+    {
+      s = '1' +s;
+    }
+    return s;
+  }
+public:
+    int numSteps(string s) {
+      int count = 0;
+        while(s.size() != 1)
+        {
+          cout << s<< endl;
+          if (s[s.length()-1] - '0' == 0)
+          {
+            s = s.substr(0,s.length()-1);
+          }
+          else
+          {
+            s = add1(s);
+          }
+          count++;
+        }
+      return count;
+    }
+};
