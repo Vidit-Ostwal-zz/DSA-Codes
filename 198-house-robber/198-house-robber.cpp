8eb42ebe29573427bc27dd4a9a665bdea1b82061
Vidit <viditostwal@gmail.com>
2022-01-27 13:22:12 +0530

    Time: 4 ms (36.33%), Space: 9.7 MB (5.31%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -14,7 +14,29 @@ public:
         return dp[n-1];
   }
   
+  
+  int recursive_solution(vector<int> nums, int index)
+  {
+    if (index >= nums.size())
+      return 0;
+    
+    return max(nums[index]+recursive_solution(nums,index+2),recursive_solution(nums,index+1));
+  }
+  
+  int memoization_solution(vector<int> nums, int index,vector<int>&dp)
+  {
+    if (index >= nums.size())
+      return 0;
+    
+    if (dp[index] != -1)
+      return dp[index];
+    
+    return dp[index] = max(nums[index]+memoization_solution(nums,index+2,dp),memoization_solution(nums,index+1,dp));
+  }
     int rob(vector<int>& nums) {
+      vector<int>dp(nums.size(),-1);
+      return memoization_solution(nums,0,dp);
+      return recursive_solution(nums,0);
         return bottom_up_solution(nums,nums.size());
     }
 };
