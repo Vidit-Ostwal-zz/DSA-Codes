19bbf08583cf71c830d82595d5e6b48d46872cdb
Vidit <viditostwal@gmail.com>
2022-01-31 23:26:16 +0530

    Time: 12 ms (5.69%), Space: 8.7 MB (92.49%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        int i = 0;
+  int switch_non_val(vector<int>&nums, int val)
+  {
+    int i = 0;
       for (int j = 0; j < nums.size();j++)
       {
         if (nums[j] != val)
@@ -11,5 +11,30 @@ public:
         }
       }
       return i;
+  }
+
+  int switch_val (vector<int>&nums, int val)
+  {
+    int i = 0;
+    int n = nums.size();
+    while (i < n)
+    {cout << i << "   " << n <<  endl;
+      if (nums[i] == val)
+      {
+        nums[i] = nums[n-1];
+        n--;
+      }
+      else
+      {
+        i++;
+      }
+     cout << i << "   " << n <<  endl;
+    }
+    return n;
+  }
+public:
+    int removeElement(vector<int>& nums, int val) {
+      return switch_val(nums,val);
+        return switch_non_val(nums,val);
     }
 };
