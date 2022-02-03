91291f1303abc7ede017796be4536f8e948d1771
Vidit <viditostwal@gmail.com>
2022-02-03 10:36:04 +0530

    Time: 12 ms (83.45%), Space: 10.9 MB (29.12%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+  /*Two Pointer Approach
+  Hash Map cannot be used because we map same values twice either use a vector of hash map
+  But that will be too much compolicated
+  We need another thing in which these two things are paired up in some manner*/
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int,int>> v;
+        for (int i = 0; i < nums.size(); i++)
+        {
+          v.push_back(make_pair(nums[i],i));
+        }
+        sort(v.begin(),v.end());
+        
+        int start = 0;
+        int end = nums.size() -1;
+        
+        while (start <= end)
+        {
+          int sum  = v[start].first + v[end].first;
+          
+          if (sum  == target)
+          {
+            vector<int> answer;
+            answer.push_back(v[start].second);
+            answer.push_back(v[end].second);
+            return answer;
+          }
+          else if (sum > target)
+            end--;
+          
+          else
+            start++;
+          
+        }
+      return {};
+    }
+};
