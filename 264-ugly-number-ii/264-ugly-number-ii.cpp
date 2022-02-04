8c30f8a850a62ea62673e0327f528a9b0b48cfac
Vidit <viditostwal@gmail.com>
2022-02-04 11:02:40 +0530

    Time: 3 ms (95.20%), Space: 9.5 MB (37.98%) - LeetHub

--- 264-ugly-number-ii/264-ugly-number-ii.cpp
+++ 264-ugly-number-ii/264-ugly-number-ii.cpp
@@ -1,62 +1,66 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        vector<int> ugly_number;
-	    ugly_number.push_back(1);
-	    
-	    int index2 = 0;
-	    int index3 = 0;
-	    int index5 = 0;
-	    
-	    int next2;
-	    int next3;
-	    int next5;
-	    int minimum_multiple = 0;
-	    for (int i =0 ;i < n-1 ; i++)
-	    {
-	    next2 = ugly_number[index2]*2;
-	    next3 = ugly_number[index3]*3;
-	    next5 = ugly_number[index5]*5;
-	    
-	    if (next2 < next3)
-	    {
-	        if (next5 < next2)
-	        {
-	            minimum_multiple = next5;
-	        }
-	        else
-	        {
-	            minimum_multiple = next2;
-	        }
-	    }
-	    else
-	    {
-	        if (next5 < next3)
-	        {
-	            minimum_multiple = next5;
-	        }
-	        else
-	        {
-	            minimum_multiple = next3;
-	        }
-	    }
-	    cout << minimum_multiple << endl;
-	    ugly_number.push_back(minimum_multiple);
-	    
-	    if (minimum_multiple == next2)
-	    {
-	        index2++;
-	    }
-	    if (minimum_multiple == next3)
-	    {
-	        index3++;
-	    }
-	    if (minimum_multiple == next5)
-	    {
-	        index5++;
-	    }
-	    
-	    }
-	    return ugly_number[n-1];
+        vector<int> ugly_numbers;
+      ugly_numbers.push_back(1);
+      
+      int index_2 = 0;
+      int index_3 = 0;
+      int index_5 = 0;
+      
+      int re_index = n;
+      while (n != 1)
+      {
+        int next_2 = 2*ugly_numbers[index_2];
+        int next_3 = 3*ugly_numbers[index_3];
+        int next_5 = 5*ugly_numbers[index_5];
+        
+        int next_multi;
+        if (next_2 < min(next_3,next_5))
+          next_multi = next_2;
+        
+        else if (next_3 <min(next_2,next_5))
+          next_multi = next_3;
+        
+        else if (next_5 < min(next_2,next_3))
+          next_multi = next_5;
+        
+        else if (next_2 == next_3)
+        {
+          if (next_2 < next_5)
+            next_multi = next_2;
+          else
+            next_multi = next_5;
+        }
+        
+        else if (next_5 == next_3)
+        {
+          if (next_5 < next_2)
+            next_multi = next_5;
+          else
+            next_multi = next_2;
+        }
+        
+        else if (next_2 == next_5)
+        {
+          if (next_2 < next_3)
+            next_multi = next_2;
+          else
+            next_multi = next_3;
+        }
+        ugly_numbers.push_back(next_multi);
+        
+        if (next_multi == next_2)
+          index_2++;
+        
+        if (next_multi == next_3)
+          index_3++;
+        
+        if (next_multi == next_5)
+          index_5++;
+        
+        n--;
+      }
+      return ugly_numbers[re_index-1];
     }
 };
