d2f61e91d42fa4d2211822ad3773d95cbbf7fa6e
Vidit <viditostwal@gmail.com>
2022-02-12 11:20:35 +0530

    Time: 171 ms (32.00%), Space: 61.8 MB (42.48%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        queue<TreeNode*> q;
+      q.push(root);
+      int answer = 0;
+      
+      while (!q.empty())
+      {
+        int size = q.size();
+        answer = 0;
+        for (int i = 0; i < size;i++)
+        {
+          TreeNode* temp = q.front();
+          q.pop();
+
+          answer += temp -> val;
+          
+          if (temp -> left)
+            q.push(temp->left);
+          if (temp -> right)
+            q.push(temp -> right);
+        }
+      }
+      return answer;
+    }
+};
