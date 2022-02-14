39ab5f403cdf111a8d2f29d85d16893a947aa5e6
Vidit <viditostwal@gmail.com>
2022-02-14 10:52:40 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (80.35%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> count(26,0);
+      vector<bool> visited(26,false);
+      
+      for (int i = 0; i < s.length(); i++)
+        count[s[i] -'a']++;
+      
+      string res = "";
+      for (int i = 0 ;i < s.length(); i++)
+      {
+        count[s[i] - 'a']--;
+        
+        if (!visited[s[i] - 'a'])
+        {
+          while (res.length() > 0 && res.back() > s[i] && count[res.back() -'a'] > 0 )
+          {
+            visited[res.back() - 'a'] = false;
+            res.pop_back();
+          }
+          
+          res += s[i];
+          visited[s[i]- 'a'] = true;
+          cout << res << endl;
+        }
+      }
+      return res;
+    }
+};
