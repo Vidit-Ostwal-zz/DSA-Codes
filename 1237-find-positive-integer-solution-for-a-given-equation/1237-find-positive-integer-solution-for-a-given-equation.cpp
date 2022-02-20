d0ce6afc2f1659cf69295405fb12f4cd1ed7b1e5
Vidit <viditostwal@gmail.com>
2022-02-20 11:44:01 +0530

    Time: 12 ms (26.27%), Space: 6.2 MB (90.02%) - LeetHub

--- 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.cpp
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.cpp
@@ -11,9 +11,28 @@
  */
 
 class Solution {
+  /*Classical Binary Search return index if found, returns -1 if not found*/
+  int Binary_Search(CustomFunction& customfunction, int st, int ed,int target,int x)
+ {
+  while (st <= ed)
+  {
+   int mid = st + (ed - st) / 2;
+    int temp = customfunction.f(x,mid);
+   if (temp == target)
+    return mid;
+    
+   if (temp > target)
+    ed = mid - 1;
+    
+   else
+    st = mid + 1;
+  }
+  return -1;
+}
+  
 public:
     vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
-      vector<vector<int>> answer;
+      /*vector<vector<int>> answer;
     for (int i = 1; i <= 1000; i++)
       for (int j = 1; j <= 1000; j++)
       {
@@ -27,5 +46,17 @@ public:
         }
       }
     return answer;
+    }*/
+      
+      /*Decide x via loop and then do a binary search for y*/
+      
+      vector<vector<int>> final_vector;
+      for (int x = 1; x <= 1000; x++)
+      {
+        int y = Binary_Search(customfunction,1,1000,z,x);
+        if (y != -1)
+          final_vector.push_back({x,y});
+      }
+      return final_vector;
     }
 };
