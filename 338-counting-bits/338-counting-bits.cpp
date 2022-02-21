15856bbe53c5f6e9c5969793631ee1daf8a90f43
Vidit <viditostwal@gmail.com>
2022-02-21 21:26:32 +0530

    Time: 7 ms (59.28%), Space: 8.7 MB (21.64%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,5 +1,5 @@
 class Solution {
-  int no_bits(int n)
+  /*int no_bits(int n)
   {
     int count = 0;
     while (n > 0)
@@ -10,13 +10,26 @@ class Solution {
       n = n >> 1;
     }
     return count;
-  }
+  }*/
   
   vector<int> get_answer(int n)
   {
     vector<int> answer;
-    for (int i = 0; i < n+1; i++)
-      answer.push_back(no_bits(i));
+    answer.push_back(0);
+    if (n == 0)
+      return answer;
+    
+    answer.push_back(1);
+    if (n == 1)
+      return answer;
+    
+    for (int i = 2; i < n+1; i++)
+    {
+      int temp = 0;
+      temp += (i & 1);
+      int j = i >> 1;
+      answer.push_back(temp + answer[j]);
+    }
     
     return answer;
   }
