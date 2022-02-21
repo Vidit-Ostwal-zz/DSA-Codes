887d7d110f3f4aa55c70382644498c448e2a52b6
Vidit <viditostwal@gmail.com>
2022-02-21 21:44:46 +0530

    Time: 29 ms (63.27%), Space: 14.1 MB (20.52%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -86,14 +86,6 @@ public:
           }
         }
       }
-      for (int i = 0; i < dp.size(); i++)
-      {
-        for (int j = 0; j < dp[0].size(); j++)
-          cout << dp[i][j] << "  ";
-        
-        cout << endl;
-      }
-      
       return dp[0];
     }
 };
