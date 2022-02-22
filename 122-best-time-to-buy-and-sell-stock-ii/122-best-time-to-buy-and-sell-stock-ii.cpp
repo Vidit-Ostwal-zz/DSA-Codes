4b2662a2b7a4e9c18e12672a301a2d01bc635781
Vidit <viditostwal@gmail.com>
2022-02-22 09:59:38 +0530

    Time: 19 ms (7.51%), Space: 13.1 MB (47.30%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -4,12 +4,9 @@ public:
         int profit = 0;
       
       for (int i = 1; i < prices.size(); i++)
-      {
         if (prices[i] - prices[i-1] > 0)
-        {
           profit += prices[i] - prices[i-1];
-        }
-      }
+      
       return profit;
     }
 };
