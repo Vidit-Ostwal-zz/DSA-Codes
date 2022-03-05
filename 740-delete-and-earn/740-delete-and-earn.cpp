af6fa9bb8861dbc0411593cf135ef9044b7d8fdb
Vidit <viditostwal@gmail.com>
2022-03-05 09:02:13 +0530

    Time: 14 ms (42.69%), Space: 10.5 MB (49.54%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -30,6 +30,7 @@ class Solution {
       return dp[index];
     
     int curr_val = nums[index]*mp[nums[index]];
+    
     if (index == nums.size()-1)
       return dp[index] = curr_val;
     
@@ -40,11 +41,13 @@ class Solution {
     2> index taken and index+1 cannot be taken bcuz of index
     3> index taken and index+1 can also be taken 
     */
+    
     bool flag = false;
     if (nums[index+1] != nums[index] + 1) flag = true;
     
     return dp[index] = max(memo_solution(nums,index+1,mp,dp),curr_val + ((flag) ? memo_solution(nums,index+1,mp,dp) : memo_solution(nums,index+2,mp,dp)));
   }
+  
   
   int top_down_solution(vector<int>nums,unordered_map<int,int> mp)
   {
@@ -54,6 +57,7 @@ class Solution {
 
     if (nums.size() == 1)
       return dp[0];
+    
     dp[1] = (nums[1] == nums[0] + 1) ? max(dp[0],nums[1]*mp[nums[1]]) : dp[0] + nums[1]*mp[nums[1]];
 
     for (int i = 2; i < nums.size();i++)
@@ -65,22 +69,27 @@ class Solution {
     }
     return dp[nums.size()-1];
   }
+  
+  
 public:
     int deleteAndEarn(vector<int>& nums) {
       
+      /*unque_vector basically makes all the number appear once*/
       vector<int> unique_vector;
+      
+      /*Maintains the frequency of the number */
       unordered_map<int,int> mp;
         for (int i = 0 ;i <  nums.size(); i++)
         {
           mp[nums[i]]++;
           if (mp[nums[i]] == 1)
-          {
             unique_vector.push_back(nums[i]);
-          }
-            
         }
+      
       sort(unique_vector.begin(),unique_vector.end());
+      
       return top_down_solution(unique_vector,mp);
+      
       vector<int> dp (nums.size(),-1);
       return memo_solution(unique_vector,0,mp,dp);
     }
