354ef82beb7d60eaedf55e5ca981b9af0b4f65e7
Vidit <viditostwal@gmail.com>
2022-03-07 08:44:41 +0530

    Time: 256 ms (60.00%), Space: 102.1 MB (5.00%) - LeetHub

--- 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
+++ 2195-append-k-integers-with-minimal-sum/2195-append-k-integers-with-minimal-sum.cpp
@@ -1,50 +1,19 @@
 class Solution {
-  /*Classical Binary Search return index if found, returns -1 if not found*/
-int Binary_Search(vector<int> nums, int st, int ed,int target)
- {
-  while (st <= ed)
-  {
-   int mid = st + (ed - st) / 2;
-    
-   if (nums[mid] == target)
-    return mid+1;
-    
-   if (nums[mid] > target)
-    ed = mid - 1;
-    
-   else
-    st = mid + 1;
-  }
-  return ed+1;
-}
 public:
     long long minimalKSum(vector<int>& nums, int k) {
+        long long ans = (((long long)k*(k+1))/2);
+      
       set<int> st(nums.begin(),nums.end());
-      nums = vector<int>(st.begin(),st.end());
       
-      sort(nums.begin(),nums.end());
-      for (int i =0; i < nums.size(); i++)
-        cout << nums[i] << " " ;
-      
-      cout << endl;
-      
-        long long m = k;
-      while (m != (k + Binary_Search(nums,0,nums.size()-1,m)))
+      for (auto i :st)
       {
-        int f = Binary_Search(nums,0,nums.size()-1,m);
-        m = (k + f);
-        cout <<f << "   "<< m << endl;
+        if (i <= k)
+        {
+          ans -= i;
+          k++;
+          ans += k;
+        }
       }
-      cout << m << endl;
-      long long answer = (m*(m+1))/2;
-      
-      int it = Binary_Search(nums,0,nums.size()-1,m);
-      cout <<it << endl;
-      for (int i =0; i < it;i++)
-      {
-        cout << answer << "  " ;
-        answer -= nums[i];
-      }
-      return answer;
+      return ans;
     }
 };
