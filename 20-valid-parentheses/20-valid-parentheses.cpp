fe5938c8cdb685913778e469346e355c4ad09cf1
Vidit <viditostwal@gmail.com>
2022-03-13 10:06:21 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (51.50%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,51 +1,44 @@
 class Solution {
 public:
     bool isValid(string s) {
-         stack <char> st;
-      for (int i = 0 ;i < s.length(); i++)
+      stack<char> st; 
+      st.push(s[0]);
+      for (int i = 1; i < s.length(); i++)
       {
-        if (s[i] == '(' || s[i] == '[' || s[i] == '{')
-        {
+        if (st.empty())
           st.push(s[i]);
-        }
+        
         else
         {
-          if (st.empty())
-          {
-            return false;
-          }
-        else if (s[i] == ')')
-        {
-          if (!st.empty() && st.top() != '(')
-          {
-            return false;
-          }
-          st.pop();
-        }
-        else if (s[i] == ']')
-        {
-          if (!st.empty() && st.top() != '[')
-          {
-            return false;
-          }
-          st.pop();
-        }
-        else if (s[i] == '}')
-        {
-          if (!st.empty() && st.top() != '{')
-          {
-            return false;
-          }
-          st.pop();
-        }
           
-      }
+        
+        if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+          st.push(s[i]);
+        
+        else if (s[i] == ')')
+          if (st.top() == '(')
+            st.pop();
+          else 
+            return false;
+        
+        else if (s[i] == '}')
+          if (st.top() == '{')
+            st.pop();
+          else 
+            return false;
+        
+        else if (s[i] == ']')
+          if (st.top() == '[')
+            st.pop();
+          else 
+            return false;
+          
+        }
       }
       
-      if (st.empty())
-      {
-              return true;
-      }
-      return false;
+      if (st.size())
+        return false;
+      
+      return true;
     }
 };
