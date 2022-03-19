2acb29e1eab7b2b8ab4ca5fe3a588d4c2e3bd9e5
Vidit <viditostwal@gmail.com>
2022-03-19 15:35:47 +0530

    Time: 737 ms (5.15%), Space: 123.8 MB (5.23%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,76 @@
+class Solution {
+  bool isPossible (vector<int> bloomday, int day, int m, int k)
+  {
+    vector<bool> bloomed;
+    for (int i = 0; i < bloomday.size(); i++)
+    {
+      if (bloomday[i] <= day)
+        bloomed.push_back(true);
+      else
+        bloomed.push_back(false);
+    }
+    
+    int st = 0;
+    while (!bloomed[st])
+      st++;
+    
+    int i = st;
+    vector<int> temp;
+    while (st < bloomed.size())
+    {
+      while (st < bloomed.size() && bloomed[st])
+        st++;
+      temp.push_back(st-i);
+      
+      while (st < bloomed.size() && !bloomed[st])
+        st++;
+      
+      i = st;
+    }
+    
+    int count = 0;
+    for (int i = 0; i < temp.size(); i++)
+      count += temp[i]/k;
+    
+    return count >= m;
+  }
+  
+  /*Classical Binary Search return index if found, returns -1 if not found*/
+int Binary_Search(vector<int> bloomday, int st, int ed, int m, int k)
+ {
+  int answer = -1;
+  while (st <= ed)
+  {
+   int mid = st + (ed - st) / 2;
+  
+    if (isPossible(bloomday,mid,m,k))
+    {
+     answer = mid;
+     ed = mid - 1;
+   }
+    
+    else{
+      st = mid + 1;
+    }
+  }
+  return answer;
+}
+public:
+    int minDays(vector<int>& bloomday, int m, int k) {
+      int maxi = INT_MIN;
+      
+      if (k == 1 && m == 1)
+      {
+        int mini = INT_MAX;
+        for (int i = 0; i < bloomday.size(); i++)
+          mini = min(mini,bloomday[i]);
+        
+        return mini;
+        
+      }
+        for (int i = 0; i < bloomday.size(); i++)
+          maxi = max(bloomday[i],maxi);
+      
+      return Binary_Search(bloomday,0,maxi, m,k);
+    }
+};
