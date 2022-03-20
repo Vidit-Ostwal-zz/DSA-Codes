29297fe62ee14ccca602689b900a402916b4ad1c
Vidit <viditostwal@gmail.com>
2022-03-20 18:59:51 +0530

    Time: 1654 ms (12.50%), Space: 201.2 MB (6.25%) - LeetHub

--- 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.cpp
+++ 2212-maximum-points-in-an-archery-competition/2212-maximum-points-in-an-archery-competition.cpp
@@ -1,42 +1,43 @@
 class Solution {
-  unordered_map<int,vector<vector<int>>> map;
-  int recursion_memo(vector<int> Bob, vector<int> Alice, int index, int num,int temp_sum)
-  {  
-    if (num == 0)
+  vector<int> answer;
+  int maxi = INT_MIN;
+  int left;
+  void recur(int numArrows, vector<int> alice, int profit,int index, vector<int> Bob)
+  {
+    if (numArrows == 0 || index >= alice.size())
     {
-      map[temp_sum].push_back(Bob);
-      return temp_sum;
+      if (maxi < profit)
+      {
+        left = numArrows;
+        maxi = profit;
+        answer = Bob;
+      }
+      return ;
     }
     
-    if (index == Alice.size())
+    recur(numArrows,alice,profit,index+1,Bob);
+    
+    int req_arrow = alice[index] + 1 ;
+    
+    if (req_arrow <= numArrows)
     {
-      map[temp_sum].push_back(Bob);  
-      return temp_sum;
+      Bob[index] = req_arrow;
+      numArrows -= req_arrow; 
+      recur(numArrows,alice,profit+index,index+1,Bob);
     }
-    if (Alice[index] +1 > num)
-      return recursion_memo(Bob,Alice,index+1,num,temp_sum);
-    
-    int option1 = recursion_memo(Bob,Alice,index+1,num,temp_sum);
-    Bob[index] = Alice[index] +1;
-    temp_sum += index;
-    int option2 = recursion_memo(Bob,Alice,index+1,num-Bob[index],temp_sum);
-    
-    return max(option1,option2);
   }
+  
 public:
     vector<int> maximumBobPoints(int numArrows, vector<int>& aliceArrows) {
-        vector<int> BobArrows(aliceArrows.size(),0);
+      vector<int>Bob(aliceArrows.size(),0);
       
-      vector<vector<int>> dp(aliceArrows.size()+1,vector<int>(numArrows + 1,-1));
-      int index = recursion_memo (BobArrows,aliceArrows,0,numArrows,0);
+        recur(numArrows,aliceArrows,0,0,Bob);
+      answer[0] += left;
       
-      vector<int> vec1 = map[index][0];
+      for (int i = 0; i < answer.size(); i++)
+        cout << answer[i] << "   " ;
       
-      int arrow_used = 0;
-      for (int i = 0; i < vec1.size(); i++)
-        arrow_used += vec1[i];
-      
-      vec1[0] = numArrows - arrow_used;
-      return vec1;
+      cout << endl;
+      return answer;
     }
 };
