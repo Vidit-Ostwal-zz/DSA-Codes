36127c5017bde1e5a0fa3c508b320770d1a94f22
Vidit <viditostwal@gmail.com>
2022-03-21 09:54:09 +0530

    Time: 16 ms (7.71%), Space: 7.2 MB (7.75%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,9 +1,44 @@
 class Solution {
   /*At most in one part, what more important, the last occuring index of the same 
 This is a string always you can use a 26 size vector*/
+  vector<int> Approach2(string s)
+  {
+    map<char,int>mp;
+    
+    for (int i = 0; i < s.length(); i++)
+      mp[s[i]]++;
+    
+    map<char,bool> mp1;
+    
+    int answer = 0;
+    vector<int> temp;
+    for (int i = 0; i < s.length(); i++)
+    {
+      mp[s[i]]--;
+      
+      if (mp[s[i]])
+        mp1[s[i]] = true;
+      else 
+        if (mp1.find(s[i]) != mp1.end())
+        {
+          mp1.erase(s[i]);
+        }
+      
+      if (mp[s[i]] == 0 && mp1.size() == 0)
+      {
+        temp.push_back(i-answer+1);
+        answer = i+1;
+      }
+    }
+    return temp;
+  }
+  
+  
 public:
     vector<int> partitionLabels(string s) {
-      /*We got Alpha which have the last occuring indexes of all*/  
+      /*We got Alpha which have the last occuring indexes of all*/ 
+      
+      return Approach2( s);
       vector<int> alpha(26,0);
       
       for (int i =0; i < s.length(); i++)
