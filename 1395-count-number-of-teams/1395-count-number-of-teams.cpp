edc204765b311e1ab6ade434dd5ce1c5c639a5c1
Vidit <viditostwal@gmail.com>
2022-03-24 20:09:16 +0530

    Time: 794 ms (5.05%), Space: 13.7 MB (8.71%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -9,30 +9,38 @@ public:
       for (int i = 0; i < rating.size(); i++)
       {
         for (int j = 0; j < i; j++)
+        {
           if (rating[j] < rating[i])
             mp_back[i]++;
+          else
+            mp_back1[i]++;
+        }
       }
       
       for (int i = rating.size()-1; i >= 0 ; i--)
       {
         for (int j = rating.size()-1; j > i; j--)
+        {
           if (rating[j] > rating[i])
             mp_forw[i]++;
-      }
-      
-      for (int i = 0; i < rating.size(); i++)
-      {
-        for (int j = 0; j < i; j++)
-          if (rating[j] > rating[i])
-            mp_back1[i]++;
-      }
-      
-      for (int i = rating.size()-1; i >= 0 ; i--)
-      {
-        for (int j = rating.size()-1; j > i; j--)
-          if (rating[j] < rating[i])
+          else 
             mp_forw1[i]++;
+        }
       }
+      
+//       for (int i = 0; i < rating.size(); i++)
+//       {
+//         for (int j = 0; j < i; j++)
+//           if (rating[j] > rating[i])
+//             mp_back1[i]++;
+//       }
+      
+//       for (int i = rating.size()-1; i >= 0 ; i--)
+//       {
+//         for (int j = rating.size()-1; j > i; j--)
+//           if (rating[j] < rating[i])
+//             mp_forw1[i]++;
+//       }
       
       int answer = 0;
       for (int i = 0; i < rating.size(); i++)
