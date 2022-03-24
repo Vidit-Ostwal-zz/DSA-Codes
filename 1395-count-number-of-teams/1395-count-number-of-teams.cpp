82088a4c5abb56e83534cbba9dadc86c3ca48370
Vidit <viditostwal@gmail.com>
2022-03-24 20:07:49 +0530

    Time: 384 ms (5.05%), Space: 13.6 MB (8.71%) - LeetHub

---
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int numTeams(vector<int>& rating) {
+        unordered_map<int,int> mp_forw;
+      unordered_map<int,int>  mp_back;
+       unordered_map<int,int> mp_forw1;
+      unordered_map<int,int>  mp_back1;
+      
+      for (int i = 0; i < rating.size(); i++)
+      {
+        for (int j = 0; j < i; j++)
+          if (rating[j] < rating[i])
+            mp_back[i]++;
+      }
+      
+      for (int i = rating.size()-1; i >= 0 ; i--)
+      {
+        for (int j = rating.size()-1; j > i; j--)
+          if (rating[j] > rating[i])
+            mp_forw[i]++;
+      }
+      
+      for (int i = 0; i < rating.size(); i++)
+      {
+        for (int j = 0; j < i; j++)
+          if (rating[j] > rating[i])
+            mp_back1[i]++;
+      }
+      
+      for (int i = rating.size()-1; i >= 0 ; i--)
+      {
+        for (int j = rating.size()-1; j > i; j--)
+          if (rating[j] < rating[i])
+            mp_forw1[i]++;
+      }
+      
+      int answer = 0;
+      for (int i = 0; i < rating.size(); i++)
+        answer += ((mp_forw[i]*mp_back[i]) + mp_forw1[i]*mp_back1[i]);
+      
+      return answer;
+    }
+};
