097ebfaa8b1de82f0fe57551591b3317af0747d0
Vidit <viditostwal@gmail.com>
2022-03-28 10:36:35 +0530

    Time: 12 ms (10.96%), Space: 11.4 MB (29.80%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,59 +1,60 @@
 class Solution {
-  /*Returns the Peak element in roated sorted array, point where the pivot occurs
-  If no pivot occurs than, which means it not rotated even return the first element*/
- int Binary_Search_Peak(vector<int> nums)
- {
-  int st = 0; 
-  int ed = nums.size() -1;
-   
-  while (st <= ed)
+  int rotated(vector<int> nums)
   {
-   int mid = st + (ed - st)/2;
+    int st = 0;
+    int ed = nums.size()-1;
     
-   if (mid + 1 < nums.size() && nums[mid] > nums[mid+1])
-    return mid;
-    
-   if (mid - 1 >= 0 && nums[mid-1] > nums[mid])
-    return mid-1;
-     
-   else if (nums[mid] > nums[ed])
-    st = mid + 1;
-    
-   else 
-    ed = mid - 1;
-  }
-  return st;
- }
-  
-  int Binary_Search(vector<int> nums, int st, int ed,int target)
-  {
     while (st <= ed)
     {
-      int mid = st + (ed - st) / 2;
+      int mid = (st + ((ed - st)/2));
       
-      if (nums[mid] == target)
+      if (mid+1 < nums.size() && nums[mid] > nums[mid+1])
         return mid;
       
-      if (nums[mid] >  target)
+      else if (mid-1 >= 0 && nums[mid] < nums[mid-1])
+        return mid-1;
+      
+      else if (nums[mid] <= nums[ed])
         ed = mid - 1;
       
       else
         st = mid + 1;
     }
-    return -1;
+    return st;
   }
+  
+    /*Classical Binary Search return index if found, returns -1 if not found*/
+int Binary_Search(vector<int> nums, int st, int ed,int target)
+ {
+  while (st <= ed)
+  {
+   int mid = st + (ed - st) / 2;
+    
+   while (st < ed && nums[st] == nums[st+1])
+      st++;
+    
+   while (st < ed && nums[ed] == nums[ed-1])
+      ed--;
+    
+   if (nums[mid] == target)
+    return mid;
+    
+   if (nums[mid] > target)
+    ed = mid - 1;
+    
+   else
+    st = mid + 1;
+  }
+  return -1;
+}
 public:
     int search(vector<int>& nums, int target) {
-      int pivot = Binary_Search_Peak(nums);
-      cout << pivot << endl;
+        int index = (nums.size() == 1) ? 0 : rotated(nums);
       
-      int index1 = Binary_Search(nums,0,pivot,target);
-      cout << index1 << endl;
+      int temp = Binary_Search(nums,0,index,target);
+       if (temp != -1)
+       return temp;
       
-      if (index1 == -1)
-        return Binary_Search(nums,pivot+1,nums.size()-1,target);
-      
-      return index1;
-      
+      return (Binary_Search(nums,index+1,nums.size()-1,target));
     }
 };
