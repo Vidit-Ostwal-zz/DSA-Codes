95ef2ae95ad7246ac3dfcf8d864a5e6966666e9a
Vidit <viditostwal@gmail.com>
2022-04-06 08:19:10 +0530

    Time: 1278 ms (7.82%), Space: 71.9 MB (9.50%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,45 +1,42 @@
 class Solution {
 public:
-  /*Combining Linear Search to fix the first element and then performing 
-  using a two pointer approach to get other two indexes
-  Sorting is important*/
     vector<vector<int>> threeSum(vector<int>& nums) {
-      
-      if (nums.size() < 3)
-        return {};
-      
+        unordered_map<int,vector<int>> map;
       sort(nums.begin(),nums.end());
-      int target = 0;
       
-      set<vector<int>> set1;
-      for (int i = 0; i < nums.size()-2; i++)
+      for (int i = 0; i < nums.size(); i++)
+        map[nums[i]].push_back(i);
+      
+      set<vector<int>> answer; 
+      for (int i = 0 ;i < nums.size(); i++)
       {
-        int first = nums[i];
-        
-        int start = i+1;
-        int end = nums.size()-1;
-        
-        while (start < end)
+        for (int j = i+1 ; j < nums.size(); j++)
         {
-          int sum = first + nums[start] + nums[end];
-          if ( sum == target)
+          int left = -1 * (nums[i] + nums[j]);
+          
+          for (int k = 0; k < map[left].size(); k++)
           {
-            vector<int> temp = {first,nums[start],nums[end]};
-            set1.insert(temp);
-            start++;
-            end--;
-          }
-          else if (sum > target)
-          {
-            end--;
-          }
-          else
-          {
-            start++;
+            if (map[left][k] > j)
+            {
+              vector<int> f;
+              f.push_back(nums[i]);
+              f.push_back(nums[j]);
+              f.push_back(nums[map[left][k]]);
+              answer.insert(f);
+              
+              while (j +1 < nums.size() && nums[j] == nums[j+1])
+                j++;
+              
+              break;
+            }
           }
         }
+        while (i +1 < nums.size() && nums[i] == nums[i+1])
+                i++;
+        
+     
       }
-      vector<vector<int>> answer(set1.begin(),set1.end());
-      return answer;
+      vector<vector<int>> final_a(answer.begin(),answer.end());
+      return final_a;
     }
 };
