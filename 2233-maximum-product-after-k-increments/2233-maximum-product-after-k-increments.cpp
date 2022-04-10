d9095a0b1874b140ed3e203c4c4bbe7216d97226
Vidit <viditostwal@gmail.com>
2022-04-10 10:22:00 +0530

    Time: 1029 ms (10.00%), Space: 90.8 MB (10.00%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
@@ -4,9 +4,7 @@ public:
         priority_queue<int,vector<int>,greater<int>> pq;
       
       for (int i=  0; i < nums.size(); i++)
-      {
         pq.push(nums[i]);
-      }
       
       while (k)
       {
