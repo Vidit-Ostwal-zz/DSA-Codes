78878d8541a9e38478e2a4db820c35fcfe2860c2
Vidit <viditostwal@gmail.com>
2022-04-10 10:21:45 +0530

    Time: 700 ms (20.00%), Space: 90.9 MB (10.00%) - LeetHub

---
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums, int k) {
+        priority_queue<int,vector<int>,greater<int>> pq;
+      
+      for (int i=  0; i < nums.size(); i++)
+      {
+        pq.push(nums[i]);
+      }
+      
+      while (k)
+      {
+        int temp = pq.top();
+        pq.pop();
+        temp++;
+        pq.push(temp);
+        k--;
+      }
+      long long answer = 1;
+      
+      while (!pq.empty())
+      {
+        // cout << pq.top() << "  " << endl;
+        answer *= pq.top();
+        answer = answer%1000000007;
+        // cout << answer << endl;
+        pq.pop();
+      }
+      return answer;
+    }
+};
