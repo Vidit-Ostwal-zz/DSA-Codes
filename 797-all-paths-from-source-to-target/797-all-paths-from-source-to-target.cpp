b851e3085750dae728fe819482679cf755c35313
Vidit <viditostwal@gmail.com>
2022-04-14 17:33:27 +0530

    Time: 35 ms (30.23%), Space: 18.4 MB (14.38%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -0,0 +1,41 @@
+class Solution {
+  vector<vector<int>> answer;
+  // unordered_map<int,vector<int>> u1;
+  
+  void BFS (vector<vector<int>> graph)
+  {
+    vector<bool> visited(graph.size(),true);
+    queue<pair<int,vector<int>>> q;
+    
+    vector<int> temp;
+    temp.push_back(0);
+    q.push(make_pair(0,temp));
+    visited[0] = false;
+    while (!q.empty())
+    {
+      auto it = q.front();
+      q.pop();
+      
+      int i = it.first;
+      
+      if (i == graph.size()-1)
+        answer.push_back(it.second);
+      
+      for (int j = 0; j < graph[i].size(); j++)
+      {
+        if (visited[graph[i][j]])
+        {
+          it.second.push_back(graph[i][j]);
+          q.push(make_pair(graph[i][j],it.second));
+          it.second.pop_back();
+        }
+      }
+    }
+  }
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+      // make_map (graph);
+      BFS(graph);
+      return answer;
+    }
+};
