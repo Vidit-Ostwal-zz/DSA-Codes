6665e3e42e695c667aec22e6ccd38ab710de093b
Vidit <viditostwal@gmail.com>
2022-04-29 10:03:17 +0530

    Time: 15 ms (37.52%), Space: 10.5 MB (6.58%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -5,26 +5,20 @@ public:
       vector<vector<int>> dp (m+1,vector<int>(n+1,-1));
     
     for (int i = 0; i <= m; i++)
-    {
       for (int j = 0; j <= n; j++)
       {
         if (i == 0 || j == 0)
         {
           int temp = 0;
           if (i > 0)
-          {
             temp = dp[i-1][j];
-          }
           else if (j > 0)
-          {
             temp = dp[i][j-1];
-          }
           dp[i][j] = grid[i][j]+ temp;
         }
         else
           dp[i][j] = grid[i][j] + min(dp[(i-1)][j],dp[i][j-1]);
       }
-    }
     return dp[m][n];
   }
   
