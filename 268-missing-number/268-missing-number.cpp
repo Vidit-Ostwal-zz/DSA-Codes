75f26085ea7acdef959ef900318719a2c738e786
Vidit <viditostwal@gmail.com>
2022-05-29 10:52:03 +0530

    Time: 43 ms (13.69%), Space: 17.9 MB (65.00%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-   /*
-  Total there will be nums.size() + 1 numbers
-  In sorted version it would be it at it's position like 1 will be at it's 1 index
-  What about the number which is equal to nums.size () ignore it */
   
   void swap (int &A, int &B)
   {
@@ -17,17 +13,13 @@ public:
       int i = 0;
       while (i < nums.size())
       {
-        /*correct_index is the right index for this value*/
         int correct_index = nums[i];
-        /*if it's present over the right index then increase i otherwise swap*/
+        
         if (nums[i] < nums.size() && nums[i] != nums[correct_index])
-        {
           swap(nums[i],nums[correct_index]);
-        }
+        
         else
-        {
           i++;
-        }
       }
       
       i = 0;
