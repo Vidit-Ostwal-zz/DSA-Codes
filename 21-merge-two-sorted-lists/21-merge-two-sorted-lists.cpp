878daa2a686a8279e5b83b9641c68b076c12ba05
Vidit <viditostwal@gmail.com>
2022-06-06 10:14:16 +0530

    Time: 23 ms (5.72%), Space: 14.8 MB (81.82%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -8,86 +8,53 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
-
-
-/* Time Complexity = O(N+M), comparing all the linked list data one by one. 
-Making a new linked list of it
-Space Complexity = O(N+M)
-*/
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* node1, ListNode* node2) {
-      /* check the first two base cases 
-      Not required but a good practise */
-      if (node1 == NULL )
-      {
-        return node2;
-      }
-      if (node2 == NULL )
-      {
-        return node1;
-      }
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
       
-      ListNode* finalpointer = new ListNode();
-      ListNode* finaldefinedpointer = finalpointer;
+      // Base Cases
+      if (list1 == NULL)
+          return list2;
       
-      /*Traversing such that one become entry */
-      while (node1 != NULL && node2 != NULL)
+      if (list2 == NULL)
+        return list1;
+      
+      // Making Arrangments such that list1 head is the head;
+      ListNode* A = list1;
+      ListNode* B = list2;
+      ListNode* temp;
+      ListNode* Head;
+      if (A-> val > B-> val)
       {
-        if (node1 -> val  <= node2 -> val)
+        temp = A;
+        A = B;
+        B = temp;
+      }
+      // Now smallest is ListNode A
+      temp = A;
+      Head = temp;
+      A = A -> next;
+      while (A && B)
+      {
+        if (A -> val < B -> val)
         {
-          finalpointer -> val = node1 -> val;
-          finalpointer -> next = new ListNode();
-          node1 = node1 -> next;
-          finalpointer = finalpointer -> next;
+          temp-> next = A;
+          A = A -> next;
         }
         else
         {
-          finalpointer -> val = node2 -> val;
-          finalpointer -> next = new ListNode();
-          node2 = node2 -> next;
-          finalpointer = finalpointer -> next;
+          temp -> next = B;
+          B = B-> next;
         }
+        temp = temp -> next;
       }
       
-      /* one becomes empty and adding other to it 
-      2 cases */
-      if (node1 == NULL && node2 != NULL)
-        {
-          while (node2 != NULL)
-          {
-            finalpointer -> val = node2 -> val;
-            node2 = node2 -> next;
-            if (node2 == NULL)
-            {
-              finalpointer-> next = NULL;
-            }
-            else
-            {
-              finalpointer -> next = new ListNode();
-              finalpointer = finalpointer -> next;
-            }
-          }
-        }
-        
-      if (node2 == NULL && node1 != NULL)
-        {
-          while (node1 != NULL)
-          {
-            finalpointer -> val = node1 -> val;
-            node1 = node1 -> next;
-            if (node1 == NULL)
-            {
-              finalpointer-> next = NULL;
-            }
-            else
-            {
-              finalpointer -> next = new ListNode();
-              finalpointer = finalpointer -> next;
-            }
-          }
-        }
-      return finaldefinedpointer;
+      if (A)
+        temp -> next = A;
+      
+      if (B)
+        temp -> next = B;
+      
+      return Head;
     }
 };
