bd0232eb559f4fdd6e80f50de8a6759cc5bb8d78
Vidit <viditostwal@gmail.com>
2022-06-14 21:55:08 +0530

    Time: 71 ms (26.34%), Space: 30.1 MB (25.96%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,41 +1,62 @@
 class Solution {
-  vector<vector<double>> dp;
-//   int RA(string s, string t, int i, int j)
-//   {
-//     if (j == t.length())
-//       return 1;
+  set<vector<int>> set_of_answers; 
+  void Recursive_Approach(string s, string t, int indexS, int indexT , vector<int> temp)
+  {
+    if (indexT == t.length())
+    {
+      if (temp.size() == t.length())
+      {
+        // show_vector(temp);
+        set_of_answers.insert(temp);
+      }
+      return;
+    }
     
-//     if (i == s.length())
-//       return 0;
+    if (indexS == s.length())
+      return;
     
-//     if (dp[i][j] != -1)
-//       return dp[i][j];
+    if (s[indexS] == t[indexT])
+    {
+      temp.push_back(indexS);
+      Recursive_Approach(s,t,indexS+1,indexT+1,temp);
+      temp.pop_back();
+    }
     
-//     return dp[i][j] = (((s[i] == t[j]) ? RA(s,t,i+1,j+1) : 0) + RA(s,t,i+1,j));
-    
-//   }
+    Recursive_Approach(s,t,indexS+1,indexT,temp);
+    return;
+  }
 public:
     int numDistinct(string s, string t) {
-      dp = vector<vector<double>> (s.length()+1, vector<double>(t.length()+1,0));
-        // return RA(s,t,0,0);
+      //   Recursive_Approach(s,t,0,0,{});
+      // return set_of_answers.size();
       
-      for (int i = 0; i < dp.size(); i++)
+      
+      // DP will store the answer till now;
+      
+      vector<vector<double>> DP(s.length()+1,vector<double>(t.length()+1,0));
+      
+      
+      // i-1 is representing the index of string s
+      // j-1 is representing the index of string t;
+      for (int i = 0; i < DP.size(); i++)
       {
-        for (int j = 0; j < dp[0].size(); j++)
+        for (int j = 0; j < DP[0].size(); j++)
         {
-          if (j > i )
-            dp[i][j] = 0;
+          // desired string is empty
+          if (j == 0)
+            DP[i][j] = 1;
           
-          else if (j == 0)
-            dp[i][j] = 1;
+          // this cannot satisfy our conditions
+          else if (j > i)
+            DP[i][j] = 0;
+          
+          else if (s[i-1] == t[j-1])
+            DP[i][j] = DP[i-1][j] + DP[i-1][j-1];
           
           else
-            dp[i][j] = dp[i-1][j] + ((s[i-1] == t[j-1]) ? dp[i-1][j-1] : 0);
-          
-          // cout << dp[i][j] << "  ";
+            DP[i][j] = DP[i-1][j];
         }
-        // cout << endl;
       }
-      return dp[s.length()][t.length()];
+      return DP[s.length()][t.length()];
     }
 };
