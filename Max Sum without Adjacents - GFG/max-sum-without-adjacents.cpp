8991604740f28441750d856b5fb88ef5b171dd7b
Vidit <viditostwal@gmail.com>
2022-06-25 08:30:58 +0530

    Added solution - LeetHub

--- Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
+++ Max Sum without Adjacents - GFG/max-sum-without-adjacents.cpp
@@ -8,56 +8,18 @@ using namespace std;
 //User function template for C++
 class Solution{
 public:	
-    
-    /*
-    Time Complexity = O(2^N)
-    Space Complexity = O(N) Recusion stack Space*/
-    int recursive_solution (int *arr, int n, int index)
-    {
-        if (index >= n)
-        return 0;
-        
-        return max(*(arr+index) + recursive_solution(arr,n,index+2), recursive_solution(arr,n,index+1));
-    }
-    
-    
-    
-    /*
-    Time Complexity = O(N)
-    Space Complexity = O(N) Recusion stack Space*/
-    int top_down_solution (int *arr, int n,int index,vector<int>&dp)
-    {
-        if (index >= n)
-        return 0;
-        
-        if (dp[index] != -1)
-        return dp[index];
-        
-        return dp[index] = max(*(arr+index) + top_down_solution(arr,n,index+2,dp), top_down_solution(arr,n,index+1,dp));
-    }
-
-
-    /*Time Complexity = O(N)
-    Space Complexity = O(N) Recusion stack Space*/
-    int bottom_up_solution (int *arr, int n)
-    {
-        vector<int> dp(n);
-        for (int i = 0; i < n ; i++)
-        {
-            if (i  < 2)
-            dp[i] = (i == 0) ? *arr : max(arr[i],dp[i-1]);
-            
-            else
-            dp[i] = max(*(arr+i) + dp[i-2], dp[i-1]);
-        }
-        return dp[n-1];
-    }
-
 	// calculate the maximum sum with out adjacent
 	int findMaxSum(int *arr, int n) {
+	    vector<int> dp(n,0);
 	    
-	    /*vector<int> dp(n,-1);*/
-	    return bottom_up_solution(arr, n);
+	    dp[0] = arr[0];
+	    dp[1] = max(arr[0],arr[1]);
+	    
+	    for (int i = 2; i < dp.size(); i++)
+	    {
+	        dp[i] = max(dp[i-1],arr[i] + dp[i-2]);
+	    }
+	    return dp[n-1];
 	}
 };
 
