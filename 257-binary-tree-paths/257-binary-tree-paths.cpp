6cfdda720bdf59ea16bdfe2d1b2c59a9748e4ab8
Vidit <viditostwal@gmail.com>
2022-07-13 12:01:31 +0530

    Time: 0 ms (100.00%), Space: 14 MB (12.12%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.cpp
+++ 257-binary-tree-paths/257-binary-tree-paths.cpp
@@ -1,57 +1,32 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-  string get_string(vector<int> temp_vector)
-  {
-    string answer = to_string(temp_vector[0]);
-    
-    for (int i = 1 ; i < temp_vector.size();i++)
-    {
-      answer += "->";
-      answer += to_string(temp_vector[i]);
-    }
-    return answer;
-  }
-  
-  
-  void getpath(TreeNode* A,vector<int>& temp_vector,vector<string>&final_vector)
-    {
-        if (A == NULL)
-        return ;
-
-        temp_vector.push_back(A->val);
-
-        if (A->left == NULL && A-> right == NULL)
-        {
-          final_vector.push_back(get_string(temp_vector));
-          temp_vector.pop_back();
-          return;
-        }
-  
-        getpath(A->left,temp_vector,final_vector);
-        getpath(A->right,temp_vector,final_vector);
+    void f(TreeNode* node, vector<string>& ans,string ds){
         
-        temp_vector.pop_back();
+        
+        if(node == NULL)
+            return;
+        
+        //leaf node found
+        if(node->left == NULL && node->right == NULL){
+            ds = ds + to_string(node->val);
+            ans.push_back(ds);
+            return;
+        }
+        
+              
+        ds = ds + to_string(node->val);
+        ds = ds + "->";
+        
+        
+        f(node->left,ans,ds);
+       
+        f(node->right,ans,ds);
     }
-  
+    
     vector<string> binaryTreePaths(TreeNode* root) {
-      vector<string> final_vector;  
-      vector<int> temp_vector;
-        if (root == NULL)
-        return final_vector;
-      
-      
-        getpath(root,temp_vector,final_vector);
-        return final_vector;
+        vector<string>ans;
+        string ds;
+        f(root,ans,ds);
+        return ans;
     }
 };
