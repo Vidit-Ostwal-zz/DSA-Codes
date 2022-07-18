c70cfea53008b9850ea91f733a2d44e2b7316b9b
Vidit <viditostwal@gmail.com>
2022-07-18 11:08:14 +0530

    Time: 1213 ms (46.80%), Space: 253.5 MB (25.88%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,44 +1,71 @@
-class Solution {
-public:
-    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-      
-      for (int i = 0; i < matrix.size(); i++)
-        for (int j = 1; j < matrix[0].size(); j++)
-          matrix[i][j] += matrix[i][j-1];
-      
-      for (int i = 1; i < matrix.size(); i++)
-        for (int j = 0; j < matrix[0].size(); j++)
-          matrix[i][j] += matrix[i-1][j];
-      
-      
-      int count = 0;
-      for (int i = 0; i < matrix.size(); i++)
-      {
-        for (int j = 0; j < matrix[0].size(); j++)
+class Solution
+{
+    public:
+        int numSubmatrixSumTarget(vector<vector < int>> &matrix, int target)
         {
-          // Iterating over the starting point
-          for (int m = i; m  < matrix.size(); m++)
+
+            for (int i = 0; i < matrix.size(); i++)
+                for (int j = 1; j < matrix[0].size(); j++)
+                    matrix[i][j] += matrix[i][j - 1];
+
+            // for (int i = 1; i < matrix.size(); i++)
+            //     for (int j = 0; j < matrix[0].size(); j++)
+            //         matrix[i][j] += matrix[i - 1][j];
+
+//             int count = 0;
+//             for (int i = 0; i < matrix.size(); i++)
+//             {
+//                 for (int j = 0; j < matrix[0].size(); j++)
+//                 {
+//                    	// Iterating over the starting point
+//                     for (int m = i; m < matrix.size(); m++)
+//                     {
+//                         for (int n = j; n < matrix[0].size(); n++)
+//                         {
+//                            	// Iterating over the ending point
+//                             int answer = matrix[m][n];
+
+//                             if (i)
+//                                 answer -= matrix[i - 1][n];
+
+//                             if (j)
+//                                 answer -= matrix[m][j - 1];
+
+//                             if (i && j)
+//                                 answer += matrix[i - 1][j - 1];
+
+//                             if (target == answer)
+//                                 count++;
+//                         }
+//                     }
+//                 }
+//             }
+          
+          // unordered_map<int,int> freqmap;
+          int count = 0;
+          
+          for (int start = 0; start < matrix[0].size(); start++)
           {
-            for (int n = j; n < matrix[0].size(); n++)
+            for (int end = start; end < matrix[0].size(); end++)
             {
-              // Iterating over the ending point
-              int answer = matrix[m][n];
-              
-              if (i)
-                answer -= matrix[i-1][n];
-              
-              if (j)
-                answer -= matrix[m][j-1];
-              
-              if (i && j)
-                answer += matrix[i-1][j-1];
-              
-              if (target == answer)
-                count++;
+              unordered_map<int,int> freqmap;
+              freqmap[0]++;
+              int sum = 0;
+              for (int row = 0; row < matrix.size(); row++)
+              {
+                sum += matrix[row][end];
+                
+                if (start)
+                  sum -= matrix[row][start-1];
+                
+                count += freqmap[sum - target];
+                freqmap[sum]++;
+                
+                // cout << start << "   " << end << "    " << count << endl;
+              }
             }
           }
+          
+          return count;
         }
-      }
-      return count;
-    }
 };
