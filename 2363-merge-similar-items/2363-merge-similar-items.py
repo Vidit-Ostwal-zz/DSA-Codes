e53e17b2abe85cfb6c8c70e27d1e723fe07f70e4
Vidit <viditostwal@gmail.com>
2022-08-06 22:22:26 +0530

    Time: 74 ms (40.00%), Space: 16.7 MB (60.00%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.py
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        unordered_map<int,int> map1;
+      
+      for (int i = 0; i < items1.size(); i++)
+        map1[items1[i][0]] += items1[i][1];
+      
+      for (int i = 0; i < items2.size(); i++)
+        map1[items2[i][0]] += items2[i][1];
+      
+      vector<vector<int>> answer;
+      
+      auto it = map1.begin();
+      
+      while (it != map1.end())
+      {
+        answer.push_back({it -> first, it -> second});
+        it++;
+      }
+      
+      sort(answer.begin(),answer.end());
+      return answer;
+    }
+};
