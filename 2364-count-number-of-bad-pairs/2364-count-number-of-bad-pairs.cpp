6540e610cf5df1a246e75f73e16745e2b120852a
Vidit <viditostwal@gmail.com>
2022-08-06 23:08:49 +0530

    Time: 484 ms (25.00%), Space: 83.8 MB (100.00%) - LeetHub

--- 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
+++ 2364-count-number-of-bad-pairs/2364-count-number-of-bad-pairs.cpp
@@ -7,13 +7,15 @@ public:
       for (int i = 0; i < nums.size(); i++)
         nums[i] -= i;
       
-      unordered_map<int,vector<int>> map1;
+      unordered_map<int,int> map1;
       // vector<int> dp(nums.size(),0);
       
       for (int i = 0; i < nums.size(); i++)
       {
-        count += map1[nums[i]].size();
-        map1[nums[i]].push_back(i);
+        // count += map1[nums[i]].size();
+        count += map1[nums[i]];
+        map1[nums[i]]++;
+        // map1[nums[i]].push_back(i);
       }
       return (long long)nums.size()*(nums.size()-1)/2 - count;
     }
